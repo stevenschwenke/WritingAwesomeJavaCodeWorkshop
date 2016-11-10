d8ba1f912a8314fbfb0f5c040055b6a573cf8e23
stevenschwenke <steven@stevenschwenke.de>
2016-11-10 13:32:07 +0100

    Upgraded chapter about SerialVersionUID

--- src/test/java/de/stevenschwenke/java/writingawesomejavacodeworkshop/part1JavaLanguageAndMethods/SerialVersionUID.java
+++ src/test/java/de/stevenschwenke/java/writingawesomejavacodeworkshop/part1JavaLanguageAndMethods/SerialVersionUID.java
@@ -5,16 +5,31 @@ import org.junit.Test;
 import java.io.Serializable;
 
 /**
- * Created by schwenks on 03.11.2016.
+ * Often, the usage of the serial version UID is not understood. This chapter explains its usage.
  */
 public class SerialVersionUID {
 
-    @Test
-    public void bla() {
-        SerializableClass c = new SerializableClass();
-    }
+    /*
+     * From API of Serializable and Joshua Blochs famous "Effective Java":
+     * - Serializable = marker interface
+     * - every serializable class must have a non-argument (default) constructor. Absence of this is
+     *   only detected at runtime.
+     * - special handling possible with writeObject and readObject methods
+     * - each serializable class: version number "serialVersionUID"
+     * - during deserialization sender and receiver compatible classes?
+     * - if different serialVersionUID InvalidClassException
+     * - declaration of serialVersionUID by static, final long field serialVersionUID
+     * - if no serialVersionUID, calculation of default serialVersionUID
+     * - serialVersionUID strongly recommended
+     *
+     * - let IDE generate id for you. In IntelliJ IDEA, enable
+     * File => Settings... => Inspections => Serialization issues =>  Serializable class without 'serialVersionUID'
+     *
+     */
 
     private class SerializableClass implements Serializable {
+        // Adding attributes here will change the generated UID ...
 
+//        private static final long serialVersionUID = -6622066385258934578L;
     }
 }
